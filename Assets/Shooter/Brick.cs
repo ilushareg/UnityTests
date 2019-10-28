97c055d979e7e7829bd82210382f0d9c26e7bb7a
ilushareg <ilushareg@gmail.com>
2019-10-28 20:12:48 +0300

    adding crosshair

---
+++ Assets/Shooter/Brick.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour
+{
+    enum State
+    {
+        Idle,
+        Busy,
+        Lit,
+        Hit
+    };
+    State state = State.Idle;
+
+    // Start is called before the first frame update
+    GameObject goUnlit = null;
+    GameObject goLit = null;
+    GameObject goHit = null;
+
+    float countDown = 0.0f;
+
+    void Start()
+    {
+       
+        Transform[] transforms = transform.GetComponentsInChildren<Transform>();
+
+        foreach(Transform t in transforms)
+        {
+            if("TargetUnlit".Equals(t.gameObject.name))
+            {
+                goUnlit = t.gameObject;
+            }
+            else if("TargetLit".Equals(t.gameObject.name))
+            {
+                goLit = t.gameObject;
+            }
+            else if("TargetHit".Equals(t.gameObject.name))
+            {
+                goHit = t.gameObject;
+            }
+
+        }
+
+        Reset();
+
+    }
+
+    private void Reset()
+    {
+
+        SetIdle();
+
+    }
+
+    private void SetIdle()
+    {
+        goUnlit.SetActive(true);
+        goLit.SetActive(false);
+        goHit.SetActive(false);
+
+    }
+
+    private void SetBusy( float time )
+    {
+        countDown = time;
+
+        goUnlit.SetActive(true);
+        goLit.SetActive(false);
+        goHit.SetActive(false);
+
+    }
+
+    public bool SetTargetable(float time)
+    {
+        state = State.Lit;
+        countDown = time;
+        goUnlit.SetActive(false);
+        goLit.SetActive(true);
+        goHit.SetActive(false);
+
+        return true;
+    }
+
+    public bool SetHit()
+    {
+        state = State.Hit;
+        countDown = 2.0f;
+        goUnlit.SetActive(false);
+        goLit.SetActive(false);
+        goHit.SetActive(true);
+
+        return true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(countDown > 0)
+        {
+            countDown -= Time.deltaTime;
+        }
+
+        if(state == State.Idle)
+        {
+
+        }
+        else if (state == State.Busy)
+        {
+            if (countDown <= 0.0f)
+            {
+                SetIdle();
+            }
+        }
+        else if (state == State.Lit)
+        {
+            if (countDown <= 0.0f)
+            {
+                SetBusy(1.0f);
+            }
+        }
+        else if (state == State.Hit)
+        {
+            if (countDown <= 0.0f)
+            {
+                SetBusy(2.0f);
+            }
+
+        }
+
+    }
+}
---
+++ Assets/Shooter/FloatingTargets.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingTargets : MonoBehaviour
+{
+    // Start is called before the first frame update
+    List<Brick> bricks = null;
+    float countdown = 0.0f;
+
+    void Start()
+    {
+        Object b = Resources.Load("Brick");
+        bricks = new List<Brick>();
+
+        for (int i=0; i<7; i++)
+        {
+            for(int q=0; q<10; q++)
+            {
+                Vector3 pos = new Vector3(i, q, 0);
+
+                GameObject bO = (GameObject)Object.Instantiate(b);
+                bO.transform.position = pos;
+                bO.transform.parent = this.transform;
+                bricks.Add(bO.GetComponent<Brick>());
+            }
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(countdown>0)
+        {
+            countdown -= Time.deltaTime;
+        }
+
+        if(countdown <=0)
+        {
+
+            int idx = (int)(Random.value * bricks.Count);
+            bricks[idx].SetTargetable(3.0f);
+
+            countdown = 4.0f; 
+        }
+        
+    }
+}
