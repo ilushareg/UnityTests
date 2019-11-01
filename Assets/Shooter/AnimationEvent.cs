29acaefa2b838675d97cb4e37e7637092f85f96f
ilushareg <ilushareg@gmail.com>
2019-11-01 13:58:54 +0300

    animations are ready and blending fine

---
+++ Assets/Shooter/AnimationEvent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationEvent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    // the function to be called as an event
+    public void PrintEvent(int i)
+    {
+        print("PrintEvent: " + i + " called at: " + Time.time);
+    }
+}
