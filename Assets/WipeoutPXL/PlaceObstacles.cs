a78a0f3ce725cf1c2584e0161e81bcd97fb8d776
ilushareg <ilushareg@gmail.com>
2019-11-29 17:34:53 +0300

    2 new modes

---
+++ Assets/WipeoutPXL/PlaceObstacles.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceObstacles : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Object o = Resources.Load("Obstacle");
+        for(int i=0; i< 100; i++)
+        {
+            int pos = (int)(Random.value * 10.0f);
+            if(pos < 5)
+            { 
+                GameObject oo = (GameObject)Object.Instantiate(o, this.transform);
+                oo.transform.position = new Vector3(i, 0, pos-2);
+            }
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
